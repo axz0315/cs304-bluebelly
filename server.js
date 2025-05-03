9a3e09d449ef902e16dcffe5fafd8225a7fbdb6c
Trisha <trishatluri@gmail.com>
2025-05-02 20:50:33 -0400

    wrote search route and query, not working yet

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/', (req, res) => {
 
 app.post('/review/', async (req, res) => { //??: how to link username/user ID to the review?
     //create review and insert it into the database collection
-    let review = {restaurant: req.body.restaurant,
+    let review = {restaurant: req.body.restaurant, //to-do: user field using cookies
                 address: req.body.addr,
                 rating: req.body.rating,
                 text: req.body.review};
@@ -124,7 +124,25 @@ app.post('/review/', async (req, res) => { //??: how to link username/user ID to
 app.get('/main-feed/', async (req, res) => {
     const db = await Connection.open(mongoUri, "BlueBelly");
     let reviewsArray = await db.collection("reviews").find().toArray();
-    return res.render('feed.ejs', {reviews: reviewsArray});
+    return res.render('feed.ejs', {reviews: reviewsArray, search: 0});
+});
+
+//search
+app.get('/search/', async (req, res) => {
+    let field = req.query.field;
+    let name = req.query.name;
+    let fieldname;
+    let reviewsArray;
+    const db = await Connection.open(mongoUri, "BlueBelly");
+
+    if ((field == 0) || (field == 1)) { //if user does not choose a field, default to restaurant
+        fieldname = "restaurant";
+        reviewsArray = await db.collection("reviews").find({restaurant: /`${name}`/}).toArray();
+    } else if (field == 2) {
+        fieldname = "user";
+        reviewsArray = await db.collection("reviews").find({user: /`${name}`/}).toArray(); //to-do: implement once sessions are done
+    };
+    return res.render('feed.ejs', {reviews: reviewsArray, name: name, field: fieldname, search: 1});
 });
 
 // // ------------------------------------------------------------
